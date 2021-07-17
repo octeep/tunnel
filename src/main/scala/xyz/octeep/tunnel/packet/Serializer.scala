33b13062f948c2c921a78ad3cc07d580444b9bbe
roger <octeep@pm.me>
2021-07-18 01:00:04 +0800

    allow variance in Packet state

--- src/main/scala/xyz/octeep/tunnel/packet/Serializer.scala
+++ src/main/scala/xyz/octeep/tunnel/packet/Serializer.scala
@@ -25,7 +25,7 @@ object Serializer {
   def deserializePacket[S, T <: C2SPacket[S]](body: Array[Byte])(implicit stateTag: TypeTag[S]): Try[T] =
     deserialize[T](body).map {
       case x if stateTag.tpe <:< x.stateTag  => x
-      case x => throw new ClassCastException(s"Invalid matching state type: ${x.stateTag} ! <:< $stateTag")
+      case x => throw new ClassCastException(s"Invalid matching state type: !($stateTag <:< ${x.stateTag})")
     }
 
 }
